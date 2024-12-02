a6d4baae4dd9266f930ed28168ad7eb6d664f9fa
Lasse Fröhner <lasse@starcopter.com>
2024-12-02 15:07:47 +0100

    misc: configure containers for v3.7.1 and v4.0.0
    Update Zephyr versions and SDK in CI/CD workflows and build scripts
    
    - Added support for multiple Zephyr versions (v3.6.0, v3.7.1, v4.0.0)
      in the CI workflow.
    - Modified build scripts to use Zephyr v3.7.1 LTS and corresponding SDK
      version for both CI and development builds.

--- vanilla-zephyr/build_ci.sh
+++ vanilla-zephyr/build_ci.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash
-ZEPHYR_VERSION=v3.6.0
-MCUBOOT_VERSION=a4eda30f5b0cfd0cf15512be9dcd559239dbfc91
+ZEPHYR_VERSION=v3.7.1
+ZEPHYR_SDK_VERSION=0.16.9
+MCUBOOT_VERSION=ea2410697dd0262edec041a0ccb07fdbde7c1aff
 
 docker build \
     --build-arg "ZEPHYR_VERSION=$ZEPHYR_VERSION" \
+    --build-arg "ZEPHYR_SDK_VERSION=$ZEPHYR_SDK_VERSION" \
     --build-arg "MCUBOOT_VERSION=$MCUBOOT_VERSION" \
     -f "Dockerfile.ci" \
     -t "irnas/vanilla-zephyr-$ZEPHYR_VERSION-ci:latest" .
--- vanilla-zephyr/build_dev.sh
+++ vanilla-zephyr/build_dev.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
-ZEPHYR_VERSION=v3.6.0
-MCUBOOT_VERSION=a4eda30f5b0cfd0cf15512be9dcd559239dbfc91
+ZEPHYR_VERSION=v3.7.1
+ZEPHYR_SDK_VERSION=0.16.9
+MCUBOOT_VERSION=ea2410697dd0262edec041a0ccb07fdbde7c1aff
 BASE_IMAGE="irnas/vanilla-zephyr-$ZEPHYR_VERSION-ci:latest"
 
 docker build \
     --build-arg "ZEPHYR_VERSION=$ZEPHYR_VERSION" \
+    --build-arg "ZEPHYR_SDK_VERSION=$ZEPHYR_SDK_VERSION" \
     --build-arg "MCUBOOT_VERSION=$MCUBOOT_VERSION" \
     --build-arg "BASE_IMAGE=$BASE_IMAGE" \
     -f "Dockerfile.dev" \
